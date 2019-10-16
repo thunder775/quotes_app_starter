b44c6ea8ecd7de5986631c05695a82eaed4c97d7
McLarenCollege <hello@mclarencollege.in>
2019-10-16 12:39:23 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: QuotesPage(),
+    )
+  );
+}
+
+
+class QuotesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
